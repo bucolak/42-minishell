21b6a2092b03bfd29ed3203b6ce537abd1233d4d
Buket Çolak <bucolak@k1m39s04.42istanbul.com.tr>
2025-05-20 21:01:04 +0300

    parse okey gibi ama emin değilim

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: bucolak <bucolak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 16:22:33 by bucolak           #+#    #+#             */
-/*   Updated: 2025/05/20 18:03:45 by bucolak          ###   ########.fr       */
+/*   Updated: 2025/05/20 20:58:24 by bucolak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -274,7 +274,7 @@ int	main(int argc, char *argv[], char **envp)
 		add_history(line);
 		pipe_parse(&pipe_blocs, line);
 		parse_input(pipe_blocs);
-		print_pipes(pipe_blocs);
+		//print_pipes(pipe_blocs);
 		if (pipe_blocs->next)
 			handle_pipe(pipe_blocs, get, &env);
 		else if (pipe_blocs->acces_args->args[0])
@@ -282,10 +282,10 @@ int	main(int argc, char *argv[], char **envp)
 			if ((!has_redireciton(pipe_blocs)
 					&& is_built_in(pipe_blocs->acces_args->args[0]->str)))
 			{
-				//check_cmd_built_in(pipe_blocs, &env);
+				check_cmd_built_in(pipe_blocs, &env);
 			}
-			//else
-				//check_cmd_sys_call(pipe_blocs, &env, get);
+			else
+				check_cmd_sys_call(pipe_blocs, &env, get);
 		}
 		pipe_blocs = create_general_node(pipe_blocs->dqm);
 		free(line);
--- parser.c
+++ parser.c
@@ -6,82 +6,101 @@
 /*   By: bucolak <bucolak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 17:12:55 by bucolak           #+#    #+#             */
-/*   Updated: 2025/05/20 18:17:31 by bucolak          ###   ########.fr       */
+/*   Updated: 2025/05/20 20:58:04 by bucolak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	is_redireciton2(char *str)
+{
+    if (*str == '>' || *str == '<')
+        return (1); 
+    return (0);
+}
+
 void	parse_input(t_general *a)
 {
-	char **tmp;
-	int i;
-	int j;
-    
+	int		i;
+	int		j;
+	int		k;
+int len;
 	while (a)
 	{
+		k = 0;
 		i = 0;
 		j = 0;
-		tmp = ft_split(a->blocs, ' ');
-		while (tmp[i])
+		while (a->blocs[i])
 		{
-			a->acces_args->args[i] = create_arg(tmp[i], 0);
-			i++;
-		}
-		a->acces_args->args[i] = NULL;
-		i = 0;
-		while (a->acces_args->args[i])
-		{
-			if (a->acces_args->args[i]->str[0] == '"')
+			while (a->blocs[i] == ' ')
+				i++;
+			if (!a->blocs[i])
+				break ;
+			if (is_redireciton2(a->blocs+i) == 1)
 			{
-				j = 1;
-				while (a->acces_args->args[i]->str[j]
-					&& a->acces_args->args[i]->str[j] != '"')
-					j++;
-				if (a->acces_args->args[i]->str[j] == '"')
-				{
-					if (a->acces_args->args[i]->str[j + 1])
-						a->acces_args->args[i]->flag = 4;
-					else
-						a->acces_args->args[i]->flag = 0;
-				}
+                len = 1;
+                if ((a->blocs[i] == a->blocs[i + 1]) && (a->blocs[i] == '<' || a->blocs[i] == '>'))
+                    len = 2;
+				a->acces_args->args[k] = create_arg(ft_substr(a->blocs, i, len), 5);
+				k++;
+                i+=len;
+                continue;
 			}
-			else if (a->acces_args->args[i]->str[0] == '\'')
+			else if (a->blocs[i] == '"')
 			{
-				j = 1;
-				while (a->acces_args->args[i]->str[j]
-					&& a->acces_args->args[i]->str[j] != '\'')
-					j++;
-				if (a->acces_args->args[i]->str[j] == '\'')
+				j = ++i;
+				while (a->blocs[i] != '"' && a->blocs[i])
+					i++;
+				if (a->blocs[i] == '"' && (!a->blocs[i+1] || a->blocs[i+1] == ' '))
 				{
-					if (a->acces_args->args[i]->str[j + 1])
-						a->acces_args->args[i]->flag = 4;
-					else
-						a->acces_args->args[i]->flag = 1;
+					a->acces_args->args[k] = create_arg(ft_substr(a->blocs, j, i
+								- j), 0);
+					k++;
+                    i++;
 				}
-			}
-			else if (ft_strcmp(a->acces_args->args[i]->str, "<") == 0
-					|| ft_strcmp(a->acces_args->args[i]->str, "<<") == 0
-					|| ft_strcmp(a->acces_args->args[i]->str, ">") == 0
-					|| ft_strcmp(a->acces_args->args[i]->str, ">>") == 0)
-				a->acces_args->args[i]->flag = 5;
-			else if (a->acces_args->args[i]->str[0] != '\''
-					&& a->acces_args->args[i]->str[0] != '"')
-			{
-				j = 0;
-				while (a->acces_args->args[i]->str[j] != '\''
-					&& a->acces_args->args[i]->str[j] != '"'
-					&& a->acces_args->args[i]->str[j])
-					j++;
-				if (!a->acces_args->args[i]->str[j])
-					a->acces_args->args[i]->flag = 2;
 				else
-					a->acces_args->args[i]->flag = 4;
+				{
+                    while (a->blocs[i])
+					    i++;
+					a->acces_args->args[k] = create_arg(ft_substr(a->blocs, j, i
+								- j), 4);
+					k++;
+				}
+                continue;
 			}
-			i++;
+            else if(a->blocs[i] == '\'')
+            {
+                j = ++i;
+				while (a->blocs[i] != '\'' && a->blocs[i])
+					i++;
+				if (a->blocs[i] == '\'' && (!a->blocs[i] || a->blocs[i] == ' '))
+				{
+					a->acces_args->args[k] = create_arg(ft_substr(a->blocs, j, i
+								- j), 1);
+					k++;
+                    i++;
+				}
+				else
+				{
+					a->acces_args->args[k] = create_arg(ft_substr(a->blocs, j, i
+								- j), 4);
+					k++;
+				}
+                continue;
+            }
+            else
+            {
+                j = i;
+                while (a->blocs[i] && a->blocs[i] != ' ' && a->blocs[i] != '"' && a->blocs[i] != '\'' && a->blocs[i] != '<' && a->blocs[i] != '>')
+                    i++;
+                if (i > j)
+                    a->acces_args->args[k++] = create_arg(ft_substr(a->blocs, j, i - j), 2);
+                else
+                    i++; 
+                continue;
+            }
+            
 		}
-		free_split(tmp);
 		a = a->next;
 	}
 }
-
